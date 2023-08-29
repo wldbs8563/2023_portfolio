55618cdbf4440ce35d49346153939448c5348c5a
jiyun <wldbs8563@gmail.com>
2023-08-29 18:10:02 +0900

    상담전수정

--- maisonkitsune/script/product_list.js
+++ maisonkitsune/script/product_list.js
@@ -3,10 +3,11 @@ const txt = document.querySelectorAll('.info')
 console.log(item, txt)
 item.forEach(function(t,i){
     t.addEventListener('mouseover',function(){
-        txt[i].style.backgroundColor='rgba(255, 255, 255, 0.5)'
+        txt[i].style.display='block'
+        txt[i].style.background='linear-gradient(358deg, #fff 5%, transparent)'
     })
     t.addEventListener('mouseout',function(){
-        txt[i].style.background='none'
+        txt[i].style.display='none'
     })
 })
 const filter = document.querySelector('.title_left .filter')
