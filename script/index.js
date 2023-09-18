7f09342d32ab75dc84e1c94385cf5d06e38f3a0b
jiyun <wldbs8563@gmail.com>
2023-09-18 17:59:55 +0900

    design

--- script/index.js
+++ script/index.js
@@ -11,3 +11,20 @@ icon.forEach(function(t,i){
         contents[i].style.display='block'
     })
 })
+
+const design_img = document.querySelectorAll('.design_img')
+const d_contents = document.querySelectorAll('.d_contents > a')
+for(let i of design_img){i.style.display='none'}
+design_img[0].style.display='flex'
+
+d_contents.forEach(function(t,i){
+    t.addEventListener('click',function(e){
+        e.preventDefault()
+        for(let j of d_contents){j.classList.remove('d_active')}
+        t.classList.add('d_active')
+        for(let n of design_img){n.style.display='none'}
+        design_img[i].style.display='block'
+    })
+})
+
+
