fa690bab0366abbbfa7149ff5fd0338810649791
ej <solbi7878@naver.com>
2023-09-19 15:47:24 +0900

    피드백

--- script/index.js
+++ script/index.js
@@ -23,8 +23,23 @@ d_contents.forEach(function(t,i){
         for(let j of d_contents){j.classList.remove('d_active')}
         t.classList.add('d_active')
         for(let n of design_img){n.style.display='none'}
-        design_img[i].style.display='block'
+        design_img[i].style.display='flex'
     })
+})
+const popup_design = document.querySelector('.popup_design')
+const popup_img = document.querySelector('.popup_design img')
+const design_a = document.querySelectorAll('.design_img a')
+console.log(popup_design, popup_img , design_a)
+popup_design.style.display='none'
+design_a.forEach(function(t,i,a){
+    t.addEventListener('click',function(e){
+        e.preventDefault()
+        popup_design.style.display='block'
+        popup_img.src=`./images/design${i+1}.jpg`
+    })
+})
+popup_design.addEventListener('click',function(){
+    popup_design.style.display='none'
 })
 
 
